f27ca9277f08697f35f2f804f6c12bf2a7147967
shsahiti <shsahiti@hotmail.com>
2025-11-11 22:41:46 -0500

    finished HR,Sound,SPO2,

--- sketch_nov7a/sketch_nov7a.ino
+++ sketch_nov7a/sketch_nov7a.ino
@@ -6,22 +6,30 @@
 
 #define HR_AVG_WINDOW 8
 #define SPO2_AVG_WINDOW 2
+#define SOUND_AVG_WINDOW 4
 #define SAFE_HR_MAX 110
 #define SAFE_HR_MIN 40
 #define LCD_HEIGHT 2
 #define LCD_WIDTH 16
+#define SAFE_SPO2_MIN 85
+#define SAFE_SOUND_MAX 670
 
 // Global Scope Variables
 int lightSensorPin{A0};
 int soundSensorPin{A1};
 int hrBuffer[HR_AVG_WINDOW]{};
 int hrIndex{};
+bool hrBufferFilled{};
+uint32_t avgHR{};
 int spo2Buffer[SPO2_AVG_WINDOW]{};
 int spo2Index{};
-bool hrBufferFilled{};
 bool spo2BufferFilled{};
-uint32_t avgHR{};
 uint32_t avgSpO2{};
+int soundBuffer[SOUND_AVG_WINDOW]{};
+int soundIndex{};
+bool SoundBifferFilled{};
+uint32_t avgSound{};
+
 DFRobot_MAX30102 oximeter;
 rgb_lcd lcd;
 const int colorR{255};
@@ -54,6 +62,7 @@ void setup() {
   pinMode(lightSensorPin, INPUT);
   pinMode(soundSensorPin, INPUT);
 
+
   oximeter.sensorConfiguration(
     0xff, SAMPLEAVG_8, MODE_MULTILED, SAMPLERATE_200, 
     PULSEWIDTH_411, ADCRANGE_16384
@@ -66,15 +75,23 @@ void setup() {
 void loop() {
   uint32_t light, sound;
 
-  readLight(light);
-  readSound(sound);
-
   readAndDisplayData();
+  // unsafe funcion that just returns true based off "Logical" numbers
+    
+  if (SAFE_HR_MIN <= getAverageHR() <= SAFE_HR_MAX){
+    Serial.print("The heart rate is at a dangerous level, "); Serial.print(avgHR);
+  }
+  if (getAverageSpO2() <= SAFE_SPO2_MIN){
+    Serial.print("The SPO2 is too low, "); Serial.print(avgSpO2); 
+  }
+  if (getAverageSound >= SAFE_SOUND_MAX){
+    Serial.print("The sound is too loud, "); Serial.print(avgSound); 
+  }
 
-  Serial.print(" | Sound: "); Serial.print(sound);
-  Serial.print(" | Light: "); Serial.println(light);
 
-  if (/*unsafe(temp, press, hr, spo2, sound, light)*/ false)
+
+
+  }
 
   delay(100);
 }
@@ -106,6 +123,15 @@ void addSpO2(int spo2) {
   return;
 }
 
+void addSound(int sound) {
+  soundBufferBuffer[soundIndex] = sound;
+  soundIndex = (soundIndex + 1) % SOUND_AVG_WINDOW;
+  if (soundIndex == 0) SoundBifferFilled = true;
+
+  return;
+}
+
+
 // Get The Average Of All Valid Values In The Buffer
 uint32_t getAverageHR() {
   int count{hrBufferFilled  ? HR_AVG_WINDOW : hrIndex};
@@ -123,11 +149,25 @@ uint32_t getAverageSpO2() {
   return sum/count;
 }
 
+uint32_t getAverageSound() {
+  int count{SoundBifferFilled ? SOUND_AVG_WINDOW : soundIndex};
+  if (count == 0) return 0;
+  uint32_t sum{};
+  for (int i{}; i<count; i++) sum += soundBuffer[i];
+  return sum/count;
+}
+
+
 void readAndDisplayData() {
   int32_t spo2, heartRate;
   int8_t spo2Valid, heartRateValid;
 
   oximeter.heartrateAndOxygenSaturation(&spo2, &spo2Valid, &heartRate, &heartRateValid);
+
+  readLight(light);
+  readSound(sound);
+  Serial.print(" | Sound: "); Serial.print(sound);
+  Serial.print(" | Light: "); Serial.println(light);
 
   if (heartRateValid) {
     addHeartRate(heartRate);
